9fe238a2308d6e1856e29235851f339e179fc5e5
Thiago Menezes <thimenezes19@gmail.com>
2025-05-04 17:27:10 -0300

    fix: fix cliente

--- vendas/forms.py
+++ vendas/forms.py
@@ -240,7 +240,7 @@ class ComprovantesClienteForm(forms.ModelForm):
         
         if self.instance and self.instance.pk:
             if user and not user.has_perm('vendas.can_edit_finished_sale'):
-                if self.instance.clientes.analise_credito and self.instance.clientes.analise_credito.status == 'EA':
+                if self.instance.cliente.analise_credito and self.instance.cliente.analise_credito.status == 'EA':
                     self.fields['documento_identificacao_frente'].disabled = True
                     self.fields['documento_identificacao_verso'].disabled = True
                     self.fields['comprovante_residencia'].disabled = True
